ec41b235e7858973a41cb1f531acdff7d67e6ce5
singfungcheung <singfung.cheung@gmail.com>
2024-03-06 19:07:33 -0500

    encapsulation

---
+++ src/Encap_Person.java
@@ -0,0 +1,10 @@
+public class Encap_Person
+{
+    public static void main(String[] arg)
+    {
+        Person john = new Person();
+        john.setName("John");
+        System.out.println("My name is: " + john.getName());
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,16 @@
+public class Person
+{
+    private String name; // private = restricted access
+
+    // Getter
+    public String getName()
+    {
+        return this.name;
+    }
+
+    // Setter
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+}
